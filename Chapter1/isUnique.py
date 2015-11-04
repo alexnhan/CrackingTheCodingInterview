fc1fbab4ff26f315aeaefc79c9d8a3135a303b59
Alexander Nhan <alexandernhan@Alexanders-MacBook-Pro.local>
2015-11-04 11:25:37 -0500

    Chapter1

---
+++ Chapter1/isUnique.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def isUnique(str):
+	s=sorted(str);
+	for i in range(1,len(s)):
+		if(s[i]==s[i-1]):
+			return False
+	return True
+
+if __name__ == "__main__":
+	str = "hello"
+	print isUnique(str)
